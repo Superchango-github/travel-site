514f716223bb9abe0cf7750d44306394149a8394
Mauro <mauro.kruuse@cba.gov.ar>
2021-06-24 15:29:23 -0300

    Completed large hero block and intro content block.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+//var gulp = require('gulp');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+/*no es mas necesaria cap 017
+gulp.task('default', function(){
+  console.log("Hooray - you created a Goulp task.");
+});
+
+gulp.task('html', function(){
+  console.log("Imagine something useful being donde to your HTML here.");
+});*/
+
+
